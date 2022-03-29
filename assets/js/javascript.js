e27fe92f38cebda70579bf950355f7d12e738c72
NicoleD1999 <nmd1018@yahoo.com>
2022-03-29 18:07:56 -0500

    initial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,4 @@
+//set textArea's to match time displayed to the left
+//if textarea is is past actual time set class to .past
+//if text area is before actual time set class to .future
+//if text area is within the same hour as actual time set class to .present
